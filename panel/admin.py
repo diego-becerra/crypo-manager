75caa0be4f2565170d8f813dc48f7c7075fc0266
Diego <diegoc327@hotmail.com>
2020-10-02 19:10:18 -0600

    register models in admin

--- panel/admin.py
+++ panel/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Exchange
+from .models import Coin
+from .models import Market
+from .models import Wallet
 
-# Register your models here.
+admin.site.register(Exchange)
+admin.site.register(Coin)
+admin.site.register(Market)
+admin.site.register(Wallet)
